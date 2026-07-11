09fbf5b0997cd3737c4371cbf982275368e0c7e0
wasmrsbcf <wasmrsbcf@hotmail.com>
2026-07-11 21:51:55 +0630

    Update UUID environment variable in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { Buffer } = require('buffer');
 const { exec } = require('child_process');
 
 // 环境变量
-const UUID = process.env.UUID || 'c596f66f-2d64-4a72-a5d5-8c2d5b8ac5cf'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
+const UUID = process.env.UUID || 'ec4c8a79-3c5b-4c4c-801a-2cc324eb061e'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
